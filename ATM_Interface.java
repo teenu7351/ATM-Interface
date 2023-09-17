17138eaefeac8bd3d909aceddc03470d010d2fa9
Teenu Anand <126483430+teenu7351@users.noreply.github.com>
2023-09-17 22:46:25 +0530

    Update ATM_Interface.java

--- ATM_Interface.java
+++ ATM_Interface.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class ATMInterface {
     private static Scanner scanner = new Scanner(System.in);
     private static double accountBalance = 1000.0; // Initial account balance
-
     public static void main(String[] args) {
         while (true) {
             System.out.println("ATM Interface");
@@ -12,7 +10,6 @@ public class ATMInterface {
             System.out.println("3. Withdraw");
             System.out.println("4. Exit");
             System.out.print("Enter your choice: ");
-
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
@@ -35,11 +32,9 @@ public class ATMInterface {
             }
         }
     }
-
     private static void checkBalance() {
         System.out.println("Your account balance: $" + accountBalance);
     }
-
     private static void deposit() {
         System.out.print("Enter the amount to deposit: $");
         double amount = scanner.nextDouble();
@@ -50,7 +45,6 @@ public class ATMInterface {
             System.out.println("Invalid amount. Please enter a positive amount.");
         }
     }
-
     private static void withdraw() {
         System.out.print("Enter the amount to withdraw: $");
         double amount = scanner.nextDouble();
